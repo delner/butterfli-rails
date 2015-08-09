d6270248320a603fc76f6766c872487d927b0a49
David Elner <david@davidelner.com>
2015-08-08 23:48:26 -0400

    Added: Basic test for the Instagram geography subscription controller (mostly a placeholder)

---
+++ spec/controllers/instagram/subscription/geography_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+RSpec.describe Butterfli::Instagram::Subscription::GeographyController, type: :controller do
+  routes { Butterfli::Rails::Engine.routes }
+
+  # Define expected behaviors for each endpoint:
+  describe "#setup" do
+    context "when given no parameters" do
+      it_behaves_like "a well-formed HTML request", :setup, {}
+    end
+  end
+end

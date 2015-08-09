c1ae6517899ad05a8bf2d4fe13611038c645e4ce
David Elner <david@davidelner.com>
2015-08-09 18:28:08 -0400

    Added: Placeholder spec for Provider configuration

---
+++ spec/butterfli/rails/configuration/provider_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe Butterfli::Rails::Configuration::Provider do
+  # TODO: Fill this out with more detail
+  it { expect(Butterfli::Rails::Configuration::Provider).to_not be_nil }
+end

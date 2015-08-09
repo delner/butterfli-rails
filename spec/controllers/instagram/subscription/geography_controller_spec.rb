c48999257a0f6a0877846b1adbf036387798ee07
David Elner <david@davidelner.com>
2015-08-09 18:05:33 -0400

    Removed: Butterfli initializer for dummy app (extracted into spec)

--- spec/controllers/instagram/subscription/geography_controller_spec.rb
+++ spec/controllers/instagram/subscription/geography_controller_spec.rb
@@ -3,6 +3,16 @@ require 'spec_helper'
 RSpec.describe Butterfli::Instagram::Subscription::GeographyController, type: :controller do
   routes { Butterfli::Rails::Engine.routes }
 
+  # Configure the Instagram endpoint...
+  before do
+    Butterfli.configure do |config|
+      config.provider :instagram do |provider|
+        provider.client_id = "client_id"
+        provider.client_secret = "client_secret"
+      end
+    end
+  end
+
   # Define expected behaviors for each endpoint:
   describe "#setup" do
     context "when given no parameters" do
--- spec/dummy/config/initializers/butterfli.rb
+++
@@ -1,6 +0,0 @@
-Butterfli.configure do |config|
-  config.provider :instagram do |provider|
-    provider.client_id = "client_id"
-    provider.client_secret = "client_secret"
-  end
-end
